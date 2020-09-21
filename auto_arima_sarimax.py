87fd395cb0618075d86071631920ea25e2c764ea
bhanukad610 <bhanuka.16@cse.mrt.ac.lk>
2020-09-21 23:38:43 +0530

    first commit

---
+++ auto_arima_sarimax.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from pmdarima.arima import auto_arima
+from sklearn.impute import SimpleImputer
+
+def parser(s):
+    return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+
+df = pd.read_csv('df_vector_weather_one_hr.csv', parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+
+df['rain_1h'].replace([np.nan], value = [0], inplace = True)
+df['rain_3h'].replace([np.nan], value = [0], inplace = True)
+df['snow_1h'].replace([np.nan], value = [0], inplace = True)
+df['snow_3h'].replace([np.nan], value = [0], inplace = True)
+
+df['traffic_volume'] = df['traffic_volume'].interpolate(method ='time')
+df['speed_avg'] = df['speed_avg'].interpolate(method ='time')
+df['seconds_zone'] = df['seconds_zone'].interpolate(method ='time')
+
+df = df['2017-06-01 12:00:00':'2019-08-20 23:30:00']
+nobs = int(len(df) * 0.2)
+df_train, df_test = df[0:-nobs], df[-nobs:]
+train_size = len(df_train)
+test_size = len(df_test)
+
+train_y = df_train['traffic_volume']
+train_x = df_train[['speed_avg', 'seconds_zone', 'temp', 'pressure',
+       'humidity', 'wind_speed', 'wind_deg', 'rain_1h', 'rain_3h', 'snow_1h',
+       'snow_3h', 'clouds_all']]
+
+step_wise=auto_arima(train_y[1:], max_order=None, test='adf', m = 24, seasonal=True,
+                      max_P=6, max_D= 2, max_Q=3, 
+                     exogenous= train_x[1:],
+                      maxiter=50, alpha=0.05, 
+                      n_jobs=-1, 
+                     information_criterion='aic', 
+                      out_of_sample_size=test_size,
+                      start_p=1, start_q=1, 
+                      max_p=7, max_q=4, 
+                      start_d=0, max_d=3,
+                      trace=True, 
+                      error_action='ignore', 
+                      suppress_warnings=True,
+                      stepwise=True
+                     )
