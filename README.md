b0dff357371c747db1ddadfacf90a37c09527157
bhanukad610 <bhanuka.16@cse.mrt.ac.lk>
2020-09-22 10:28:18 +0530

    requements.txt

---
+++ README.md
@@ -0,0 +1,12 @@
+# SARIMAX model selection with auto.arima
+## Instructions
+1. Clone the repository.
+2. Open a command line/ terminal.
+3. Give following commands.
+
+```
+cd SARIMAX-for-Traffic-volume
+sudo apt update
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+```
--- auto_arima_sarimax.py
+++ auto_arima_sarimax.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from pmdarima.arima import auto_arima
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.impute import SimpleImputer
 
 def parser(s):
@@ -29,16 +30,21 @@ train_x = df_train[['speed_avg', 'seconds_zone', 'temp', 'pressure',
        'humidity', 'wind_speed', 'wind_deg', 'rain_1h', 'rain_3h', 'snow_1h',
        'snow_3h', 'clouds_all']]
 
-step_wise=auto_arima(train_y[1:], max_order=None, test='adf', m = 24, seasonal=True,
-                      max_P=6, max_D= 2, max_Q=3, 
+order = list(map(int,input("\nEnter the oder : ").strip().split()))[:3]
+seasonal_oder = list(map(int,input("\nEnter the seasonal order : ").strip().split()))[:4]
+print("\nSeasonal factor =", seasonal_oder[3])
+
+
+step_wise=auto_arima(train_y[1:], max_order=None, test='adf', m = seasonal_oder[3], seasonal=True,
+                      max_P=seasonal_oder[0], max_D= seasonal_oder[1], max_Q=seasonal_oder[2], 
                      exogenous= train_x[1:],
                       maxiter=50, alpha=0.05, 
                       n_jobs=-1, 
                      information_criterion='aic', 
                       out_of_sample_size=test_size,
-                      start_p=1, start_q=1, 
+                      start_p=order[0], start_q=order[2], 
                       max_p=7, max_q=4, 
-                      start_d=0, max_d=3,
+                      start_d=order[2], max_d=3,
                       trace=True, 
                       error_action='ignore', 
                       suppress_warnings=True,
