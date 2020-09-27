0fd40867f1098164a9932b690550169213913e2c
bhanukad610 <bhanuka.16@cse.mrt.ac.lk>
2020-09-27 16:18:18 +0530

    params

--- auto_arima_sarimax.py
+++ auto_arima_sarimax.py
@@ -30,23 +30,24 @@ train_x = df_train[['speed_avg', 'seconds_zone', 'temp', 'pressure',
        'humidity', 'wind_speed', 'wind_deg', 'rain_1h', 'rain_3h', 'snow_1h',
        'snow_3h', 'clouds_all']]
 
-order = list(map(int,input("\nEnter the oder : ").strip().split()))[:3]
-seasonal_oder = list(map(int,input("\nEnter the seasonal order : ").strip().split()))[:4]
-print("\nSeasonal factor =", seasonal_oder[3])
+# order = list(map(int,input("\nEnter the oder : ").strip().split()))[:3]
+# seasonal_oder = list(map(int,input("\nEnter the seasonal order : ").strip().split()))[:4]
+# print("\nSeasonal factor =", seasonal_oder[3])
 
 
 step_wise=auto_arima(train_y[1:], max_order=None, test='adf', m = seasonal_oder[3], seasonal=True,
-                      max_P=seasonal_oder[0], max_D= seasonal_oder[1], max_Q=seasonal_oder[2], 
+                      max_P=5, max_D= 1, max_Q=3, 
                      exogenous= train_x[1:],
-                      maxiter=50, alpha=0.05, 
+                      maxiter=40, alpha=0.05, 
                       n_jobs=-1, 
                      information_criterion='aic', 
                       out_of_sample_size=test_size,
-                      start_p=order[0], start_q=order[2], 
-                      max_p=7, max_q=4, 
-                      start_d=order[2], max_d=3,
+                      start_p=2, start_q=1, 
+                      max_p=5, max_q=3, 
+                      start_d=0, max_d=1,
                       trace=True, 
                       error_action='ignore', 
                       suppress_warnings=True,
                       stepwise=True
                      )
+print(step_wise.summary())
